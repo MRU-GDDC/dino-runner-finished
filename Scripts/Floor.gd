52a68231717dc134e014f8d2189755cca539d979
matthew-hre <whycardboard@gmail.com>
2023-09-25 18:42:06 -0600

    added project

---
+++ Scripts/Floor.gd
@@ -0,0 +1,8 @@
+extends StaticBody2D
+
+var speed = 150
+@onready var sprite = $Sprite2D
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	sprite.region_rect.position.x += speed * delta
